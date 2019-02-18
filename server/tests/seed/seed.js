97f692b2947fc934a9dc7587f097ebcd99036ef1
Lucas <lucasgf@hotmail.com>
2019-02-18 10:05:01 -0300

    Add authentication to todo requests

--- server/tests/seed/seed.js
+++ server/tests/seed/seed.js
@@ -19,16 +19,22 @@ const users = [{
   _id: userTwoId,
   email: 'jen@example.com',
   password: 'userTwoPass',
+  tokens: [{
+    access: 'auth',
+    token: jwt.sign({_id: userTwoId, access: 'auth'}, 'abc123').toString()
+  }]
 }];
 
 const todos = [{
   _id: new ObjectID(),
-  text: 'First test todo'
+  text: 'First test todo',
+  _creator: userOneId
 }, {
   _id: new ObjectID(),
   text: 'Second Test todo',
   completed: true,
-  completedAt: 333333
+  completedAt: 333333,
+  _creator: userTwoId
 }];
 
 const populateUsers = (done) => {
